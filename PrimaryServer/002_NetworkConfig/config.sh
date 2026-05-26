1b6f670d0ab7b497ecccf2ea500382ef23794247
AaronWk243 <aaronventurawk@gmail.com>
2026-05-26 16:56:33 +0200

    Update management URL in netbirdConnect function

--- PrimaryServer/002_NetworkConfig/config.sh
+++ PrimaryServer/002_NetworkConfig/config.sh
@@ -22,7 +22,7 @@ check_root() {
 
 netbirdConnect(){
     domainFQDN=$(hostname -f)
-    netbird up --setup-key "${NETBIRD_TOKEN}" --hostname "${domainFQDN}" --management-url https://netbird.valentinarosse.com
+    netbird up --setup-key "${NETBIRD_TOKEN}" --hostname "${domainFQDN}" --management-url "${NETBIRD_URL}"
 }
 
 disable_ipv6() {
